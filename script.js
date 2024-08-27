cb05ad20f21bf39fbdc4f1ce224bf3883aeab78c
Steve KAmoni <stevekamoni4@gmail.com>
2024-08-27 10:59:19 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,44 @@
+const greetText = document.querySelector(".greet");
+const Hours = document.querySelector(".hrs");
+const Minutes = document.querySelector(".min");
+const Seconds = document.querySelector(".sec");
+const button = document.querySelector(".btn");
+
+function updateTime() {
+  // update the time
+  const currentTime = new Date();
+  const hrs = currentTime.getHours();
+  const min = currentTime.getMinutes();
+  const sec = currentTime.getSeconds();
+
+  //    change the time fomrat
+  const hours = hrs.toString().padStart(2, "0");
+  const minutes = min.toString().padStart(2, "0");
+  const seconds = sec.toString().padStart(2, "0");
+
+  //  change the dislay of text
+
+  Hours.innerHTML = `${hours}   :`;
+  Minutes.innerHTML = `${minutes}  :`;
+  Seconds.innerHTML = `${seconds}`;
+}
+
+updateTime();
+
+setInterval(updateTime, 1000);
+
+button.addEventListener("click", () => {
+  const now = new Date();
+  const hour = now.getHours();
+  const minute = now.getMinutes();
+
+  const formatedMinutes = minute.toString().padStart(2, "0");
+
+  if (hour > 1 && hour < 12) {
+    greetText.textContent = `Good Morning, right now its currently ${hour} : ${formatedMinutes} AM`;
+  } else if (hour > 12 && hour < 15) {
+    greetText.textContent = `Good Afternoon, Right now its currently ${hour}: ${formatedMinutes} PM`;
+  } else if (hour > 15) {
+    greetText.textContent = `Good Eveining! Right now its currently ${hour} : ${formatedMinutes} PM`;
+  }
+});
